7e6a6cc56df795a26b3e1cfe9eb3db0a89f055a9
Jerry Krusinski <jkruzr@gmail.com>
2017-03-04 10:23:38 -0600

    added links html and auth rerouting

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,9 +1,13 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, Auth, $location) {
   $scope.data = {};
 
   Links.getAll().then(function(links) {
+    if (!Auth.isAuth()) {
+      $location.path('signin');
+    }
+
     $scope.data.links = links;
   });
 });
