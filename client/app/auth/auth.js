84b97769b0a3f1f57d53f9dd22ceeb71c85bb23b
Jerry Krusinski <jkruzr@gmail.com>
2017-03-04 11:39:12 -0600

    added user and pass signup validation

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -4,27 +4,60 @@
 angular.module('shortly.auth', [])
 
 .controller('AuthController', function ($scope, $window, $location, Auth) {
-  $scope.user = {};
+
+  $scope.user = {username: '', password: ''};
+  $scope.passCheck = '';
+  $scope.validCreds = false;
+  $scope.messgae = '';
+  $scope.showWarning = false;
+
+  var warn = function(message) {
+    if (!message) {
+      $scope.message = '';
+      $scope.showWarning = false;
+    } else {
+      $scope.message = message;
+      $scope.showWarning = true;
+    }
+  };
+
+  $scope.validateCreds = function () {
+    if ($scope.user.username.length < 8 && !!$scope.user.username) {
+      warn('Username must be at least 8 characters long.');
+    } else if ($scope.user.password.length < 8 && !!$scope.user.password) {
+      warn('Password must be at least 8 characters long.');
+    } else if ($scope.user.password !== $scope.passCheck && !!$scope.passCheck) {
+      warn('Passwords do not match.');
+    } else {
+      warn(false);
+    }
+  };
 
   $scope.signin = function () {
-    Auth.signin($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if ($scope.validCreds) {
+
+      Auth.signin($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
   };
 
   $scope.signup = function () {
-    Auth.signup($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if ($scope.validCreds) {
+
+      Auth.signup($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
   };
 });
