4b56556a46e9a3aa7d6bd4242411b565bed3d49a
Jerry Krusinski <jkruzr@gmail.com>
2017-03-04 10:27:02 -0600

    fixed signout route

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -64,7 +64,7 @@ angular.module('shortly.services', [])
 
   var signout = function () {
     $window.localStorage.removeItem('com.shortly');
-    $location.path('/signin');
+    $location.path('#/signin');
   };
 
 
