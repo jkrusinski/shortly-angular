877617e2acb38e1fc32e481c14be48a233bb3514
Jerry Krusinski <jkruzr@gmail.com>
2017-03-04 12:26:54 -0600

    I don't know how that happend

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,13 +1,15 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links, Auth, $location) {
+  if (!Auth.isAuth()) {
+    $location.path('signin');
+  }
+
   $scope.data = {};
 
   Links.getAll().then(function(links) {
-    if (!Auth.isAuth()) {
-      $location.path('signin');
-    }
-
     $scope.data.links = links;
   });
+
+  $scope.signout = Auth.signout;
 });
